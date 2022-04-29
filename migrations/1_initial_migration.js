b01373973ce014fd8d3f026f50160ea4b47a7803
danh996 <lcdanh996@gmail.com>
2022-04-30 06:55:58 +0700

    Day 15: interact with smart contract using ethers js

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-const Day3TokenSale = artifacts.require("Day3TokenSale");
-
-module.exports = function(deployer) {
-   deployer.deploy(Migrations);
-   deployer.deploy(Day3TokenSale);
-};
